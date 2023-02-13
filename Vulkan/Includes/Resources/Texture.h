84d88d76a0ac975f13a5c9927a410c80a3c223fe
Vynokris <vynokris@gmail.com>
2023-02-13 16:16:44 +0100

    :sparkles: Implemented mipmaps

--- Vulkan/Includes/Resources/Texture.h
+++ Vulkan/Includes/Resources/Texture.h
@@ -1,8 +1,10 @@
 ﻿#pragma once
+#include <cstdint>
 
 typedef struct VkImage_T*        VkImage;
 typedef struct VkDeviceMemory_T* VkDeviceMemory;
 typedef struct VkImageView_T*    VkImageView;
+typedef enum   VkFormat : int    VkFormat;
 
 namespace Core
 {
@@ -14,14 +16,16 @@ namespace Resources
     class Texture
     {
     private:
-        const char* filename = "";
-        int width  = 0;
-        int height = 0;
-        int channels = 0;
+        const char* filename  = "";
+        int         width     = 0;
+        int         height    = 0;
+        int         channels  = 0;
+        uint32_t    mipLevels = 0;
 
         VkImage        vkImage       = nullptr;
         VkDeviceMemory vkImageMemory = nullptr;
         VkImageView    vkImageView   = nullptr;
+        VkFormat       vkImageFormat;
         
     public:
         Texture() = default;
@@ -33,6 +37,12 @@ namespace Resources
         
         ~Texture();
 
+        int GetWidth () const { return width;  }
+        int GetHeight() const { return height; }
+        uint32_t GetMipLevels() const { return mipLevels; }
         VkImageView GetVkImageView() const { return vkImageView; }
+        
+    private:
+        void GenerateMipmaps(const Core::Renderer* renderer = nullptr);
     };
 }
